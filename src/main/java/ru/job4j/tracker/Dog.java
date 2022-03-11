7d51ccb4fe0b0b4cbe85c568f2cf07f056aff75e
Aleksandr <losas20@yandex.ru>
2022-03-11 23:09:35 +0600

    1.5. Поля объекта

--- src/main/java/ru/job4j/tracker/Dog.java
+++ src/main/java/ru/job4j/tracker/Dog.java
@@ -1,9 +1,33 @@
 package ru.job4j.tracker;
 
 public class Dog {
+
+    private String food;
+    private String name;
+
+    public void show(){
+        System.out.println(this.name);
+        System.out.println(this.food);
+    }
+    public void eat(String meat){
+        this.food = meat;
+    }
+    public void giveNick(String nick){
+        this.name = nick;
+    }
+
     public static void main(String[] args) {
         Dog polkan = new Dog();
+        polkan.giveNick("polkan");
+        polkan.eat("kotleta");
+        polkan.show();
         Dog sharik = new Dog();
+        sharik.giveNick("sharik");
+        sharik.eat("fish");
+        sharik.show();
         Dog zhychka = new Dog();
+        zhychka.giveNick("zhychka");
+        zhychka.eat("kotleta");
+        zhychka.show();
     }
 }
